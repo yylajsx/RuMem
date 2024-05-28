c03200135d2f3e51c08c21a8fafee00cc9339022
Pistachio <17628437143@163.com>
2024-05-29 05:48:18 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- RUMENG/src/components/NavBar.vue
+++ RUMENG/src/components/NavBar.vue
@@ -3,7 +3,8 @@ import BarsIcon from "@/components/icons/IconBars.vue";
 import AvatarIcon from "@/components/icons/IconAvatar.vue";
 import {ref} from "vue";
 
-let logoSrc = ref("src/assets/images/logo.png")
+// let logoSrc = ref("src/assets/images/logo.png")
+let logoSrc = ref("../../public/logo.png")
 let tabs = ref([{tab: "Games", link: "/"}, {tab: "Newswire", link: "/newswire"}, {tab: "Downloads", link: "/downloads"}
   , {tab: "Store", link: "/store"}, {tab: "Support", link: "/support"}])
 
